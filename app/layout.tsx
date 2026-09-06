a47da446f0b19a84fe27fccffb112cc1be32d48c
edtechevans <tim.m.evans18@gmail.com>
2026-09-06 19:11:50 +0800

    Load Data to Action course visual identity

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './premium.css';
 import './visual-polish.css';
 import './flat-learning.css';
 import './udl-course.css';
+import './data-course.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
