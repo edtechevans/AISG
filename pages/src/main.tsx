43694ded22908f912078eceac4cee4d598e03006
edtechevans <tim.m.evans18@gmail.com>
2026-09-07 07:40:08 +0800

    Mount Learning Companion in GitHub Pages build

--- pages/src/main.tsx
+++ pages/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import MyCoursesApp from '../../app/my-courses';
+import LearningCompanion from '../../app/learning-companion';
 import '../../app/globals.css';
 import '../../app/premium.css';
 import '../../app/visual-polish.css';
@@ -8,6 +9,7 @@ import '../../app/flat-learning.css';
 import '../../app/favourites.css';
 import '../../app/course-marks.css';
 import '../../app/home-hero.css';
+import '../../app/learning-companion.css';
 import { installStaticApi } from './static-api';
 
 if (typeof window !== 'undefined') {
@@ -15,6 +17,7 @@ if (typeof window !== 'undefined') {
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
       <MyCoursesApp staticMode />
+      <LearningCompanion />
     </StrictMode>,
   );
 }
